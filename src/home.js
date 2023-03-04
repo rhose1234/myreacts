e0aa319f633641e7da75453beded2d5b6171fe3f
Marvelous rose <marvelousrhose@gmail.com>
2023-03-04 23:15:27 +0100

    fff

--- src/home.js
+++ src/home.js
@@ -1,28 +1,51 @@
 import { useEffect, useState } from 'react';
-import BlogList from './blogList';
+import BlogList from './cardList';
 
 const Home = () => {
-const  [blogs, setBlogs] = useState ([
-    {title: 'my new blog', body:'i stand on the rock', author:'rose', id:1 },
-    {title: 'my new blog', body:'i stand on the rock', author:'marvelous', id:2 },
-    {title: 'my new blog', body:'i stand on the rock', author:'rose', id:3 }
+const  [cards, setCards] = useState ([
+    {
+    title: 'my new blog', 
+    body:'i stand on the rock', 
+    author:'rose',  
+    id:1 
+   },
+
+      {
+        title: 'my new blog',
+        body:'i stand on the rock',
+        author:'marvelous', 
+        id:2
+      },
+      {
+        title: 'my new blog',
+        body:'i stand on the rock',
+        author:'rose',
+        id:3 },
+
+      {
+        title: 'my new blog', 
+        body:'i stand on the rock',
+        author:'rose', 
+        id:4
+      }
   ]);
 
-    const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
   
-useEffect(() => {
-  console.log("my name is rose marvelous");
-})
     
     return ( 
 
-    <div className="Home container col-lg-6">
-      <BlogList blogs={blogs} title="All blogs are here!"  handleDelete={handleDelete}/>
-      <BlogList blogs = {blogs.filter((blog) => blog.author === "rose")} title="ROSIE'S BLOG" />
+    <div className="Home container ">
+    {cards.map((card) => (
+    <div className="row">
+    <div className="card col-lg-3 col-md-12">
+
+    <h2>{card.title}</h2>
+    <p>{card.body}</p>
+    <h6>{card.author}</h6>
     
+    </div>
+    </div>
+    ))}
     </div>
      );
     
